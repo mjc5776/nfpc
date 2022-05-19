96bc79f3ce198d41e419cc3544274b555c2cf7a5
Mike Corey <mjcorey57@gmail.com>
2022-05-19 14:45:38 -0600

    request form

--- client/src/components/Requests/NewRequest.js
+++ client/src/components/Requests/NewRequest.js
@@ -11,19 +11,27 @@ import {
 import PlayerHeader from '../PlayerHeader/PlayerHeader';
 import { useParams, useHistory } from 'react-router-dom';
 const NewRequest = () => {
-  const [interviewData, setInterviewData] = useState(null);
+  const [requestData, setRequestData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
-  const [interviewScore, setInterviewScore] = useState();
-  const [overallScore, setOverallScore] = useState();
-  const [interviewNotes, setInterviewNotes] = useState(null);
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [acctNum, setAcctNum] = useState();
+  const [pdType, setPDType] = useState(null);
+  const [pdTime, setPDTime] = useState(null);
+  const [pdQty, setPDQty] = useState(null);
+  const [pdDate, setPDDate] = useState(null);
+  const [pdLocation, setPDLocation] = useState(null);
+  const [pdNotes, setPDNotes] = useState(null);
+  const [cdType, setCDType] = useState(null);
+  const [fmv, setFMV] = useState(null);
+  const [cdNotes, setCDNotes] = useState(null);
 
   const params = useParams();
   const paramID = params.id;
   return (
     <>
       <PlayerHeader />
-
       <MDBContainer>
         <>
           <h1>Player Request</h1>
@@ -32,35 +40,52 @@ const NewRequest = () => {
             <label htmlFor='title' className='form-label scoreType'>
               Title
             </label>
-            <MDBInput />
+            <MDBInput
+                id="title"
+                onChange={(e) => {
+                    const title = e.target.value;
+                    setTitle(title);
+                }}
+            />
           </div>
           <div className='mb-2'>
             <label htmlFor='description' className='form-label scoreType'>
               Description
             </label>
-            <MDBInput />
+            <MDBInput
+                id="description"
+                onChange={(e) => {
+                    const desc = e.target.value;
+                    setDescription(desc);
+                }}
+            />
           </div>
           <div className='mb-2'>
             <label htmlFor='acctNum' className='form-label scoreType'>
               Account #
             </label>
-            <MDBInput />
+            <MDBInput
+                id="acctNum"
+                onChange={(e) => {
+                    const acctNum = e.target.value;
+                    setAcctNum(acctNum);
+                }}
+            />
           </div>
           <hr />
           <h1>Player Deliverable</h1>
           <div className='mb-3'>
-            <label htmlFor='interviewScore' className='form-label scoreType'>
+            <label htmlFor='pdType' className='form-label pdType'>
               Type:
             </label>
             <div className='col-md-1'>
               <select
                 required
                 className='form-select'
-                id='interviewScore'
-                aria-label='Default select example'
+                id='pdType'
                 onChange={(e) => {
-                  const selectedInterview = e.target.value;
-                  //setInterviewScore(selectedInterview);
+                  const selectedType = e.target.value;
+                  setPDType(selectedType);
                 }}
               >
                 <option value='' defaultValue>
@@ -78,22 +103,29 @@ const NewRequest = () => {
             </div>
           </div>
           <div className='mb-4'>
-            <div class='row'>
-              <label htmlFor='overallScore' className='form-label scoreType'>
+            <div className='row'>
+              <label htmlFor='pdTime' className='form-label pdType'>
                 Time/Qty:
               </label>
               <div className='col-md-1'>
-                <MDBInput label='Time' id='pdQty' type='number' />
+                <MDBInput 
+                    label='Time' 
+                    id='pdTime' 
+                    type='number' 
+                    onChange={(e) => {
+                        const pdTime = e.target.value;
+                        setPDTime(pdTime);
+                      }}
+                    />
               </div>
               <div className='col-sm-2'>
                 <select
                   className='form-select'
                   //value={!interviewData[0].OverallScore ? "" : interviewData[0].OverallScore }
-                  id='overallScore'
-                  aria-label='Default select example'
+                  id='pdQty'
                   onChange={(e) => {
-                    const selectedOverall = e.target.value;
-                    setOverallScore(selectedOverall);
+                    const selectedQty = e.target.value;
+                    setPDQty(selectedQty);
                   }}
                 >
                   <option value='' defaultValue>
@@ -108,37 +140,60 @@ const NewRequest = () => {
           </div>
 
           <div className='mb-4'>
-            <div class='row'>
+            <div className='row'>
               <div className='col-sm-2'>
-                <MDBDatepicker format='mm/dd/yyyy' inputToggle />
+                <MDBDatepicker 
+                    format='mm/dd/yyyy' 
+                    inputToggle 
+                    onChange={(e) => {
+                        const selectedDate = e.target.value;
+                        setPDDate(selectedDate);
+                      }}
+                    />
               </div>
             </div>
           </div>
           <div className='mb-4'>
-            <div class='row'>
+            <div className='row'>
               <div className='col-sm-4'>
-                <MDBInput label='Location' id='pdLocation' type='text' />
+                <MDBInput 
+                    label='Location' 
+                    id='pdLocation' 
+                    type='text' 
+                    onChange={(e) => {
+                        const pdLocation = e.target.value;
+                        setPDLocation(pdLocation);
+                      }}
+                    />
               </div>
             </div>
           </div>
           <div className='mb-4'>
-            <div class='row'>
+            <div className='row'>
               <div className='col-sm-12'>
-                <MDBTextArea label='Notes' id='pdNotes' rows={4} />
+                <MDBTextArea 
+                    label='Notes' 
+                    id='pdNotes' 
+                    rows={4} 
+                    onChange={(e) => {
+                        const pdNotes = e.target.value;
+                        setPDNotes(pdNotes);
+                      }}
+                    />
               </div>
             </div>
           </div>
           <hr />
           <h1>Club Deliverable</h1>
           <div className='mb-4'>
-            <div class='row'>
+            <div className='row'>
               <div className='col-sm-3'>
                 <select
                   className='form-select'
                   id='cdType'
                   onChange={(e) => {
-                    const selectedOverall = e.target.value;
-                    setOverallScore(selectedOverall);
+                    const selectedCDType = e.target.value;
+                    setCDType(selectedCDType);
                   }}
                 >
                   <option value='' defaultValue>
@@ -156,25 +211,38 @@ const NewRequest = () => {
             </div>
           </div>
           <div className='mb-4'>
-            <div class='row'>
+            <div className='row'>
               <div className='col-sm-3'>
+                  
                 <MDBInput
                   label='Fair Market Value'
                   id='cdValue'
                   type='number'
+                  onChange={(e) => {
+                    const fmv = e.target.value;
+                    setFMV(fmv);
+                  }}
                 />
               </div>
             </div>
           </div>
           <div className='mb-4'>
-            <div class='row'>
+            <div className='row'>
               <div className='col-sm-12'>
-                <MDBTextArea label='Notes' id='cdNotes' rows={4} />
+                <MDBTextArea 
+                label='Notes' 
+                id='cdNotes' 
+                rows={4} 
+                onChange={(e) => {
+                    const cdNotes = e.target.value;
+                    setCDNotes(cdNotes);
+                  }}
+                />
               </div>
             </div>
           </div>
           <hr />
-          {interviewScore && overallScore && (
+          {/* {interviewScore && overallScore && ( */}
             <MDBBtn
               className='mb-4'
               block
@@ -182,7 +250,7 @@ const NewRequest = () => {
             >
               Submit
             </MDBBtn>
-          )}
+          {/* )} */}
           <MDBBtn
             outline
             className='mb-4'
